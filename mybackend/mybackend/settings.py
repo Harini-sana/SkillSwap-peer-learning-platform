58a8d7dffcf75fd55c97107f048be0b49326c972
Harini Sana <hariniravi1509@gmail.com>
2026-03-31 20:34:28 +0530

    switched to sqlite for deployment

--- mybackend/mybackend/settings.py
+++ mybackend/mybackend/settings.py
@@ -65,13 +65,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "mybackend.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "skillswap",
-        "USER": "root",
-        "PASSWORD": "Harini@1511",
-        "HOST": "localhost",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
