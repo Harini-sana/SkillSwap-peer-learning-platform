a2ac486ca556f40493145abf3bce929f6360026b
Harini Sana <hariniravi1509@gmail.com>
2026-04-04 17:55:23 +0530

    Fixed API URLs and connected frontend to backend

--- mybackend/mybackend/settings.py
+++ mybackend/mybackend/settings.py
@@ -94,6 +94,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://helpful-raindrop-fe9288.netlify.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -101,6 +102,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://helpful-raindrop-fe9288.netlify.app"
 ]
 
 SESSION_COOKIE_SAMESITE = "Lax"
